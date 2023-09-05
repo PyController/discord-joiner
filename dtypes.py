838b24fa4d12dca06a0771884192acaa02aa7bf8
PyController <143403968+PyController@users.noreply.github.com>
2023-09-05 20:33:11 +0200

    New dtypes

--- dtypes.py
+++ dtypes.py
@@ -33,15 +33,3 @@ class OtherInfo:
         'x-super-properties': 'eyJvcyI6IldpbmRvd3MiLCJicm93c2VyIjoiRGlzY29yZCBDbGllbnQiLCJyZWxlYXNlX2NoYW5uZWwiOiJzdGFibGUiLCJjbGllbnRfdmVyc2lvbiI6IjEuMC45MDE2Iiwib3NfdmVyc2lvbiI6IjEwLjAuMTkwNDUiLCJvc19hcmNoIjoieDY0Iiwic3lzdGVtX2xvY2FsZSI6InN2IiwiYnJvd3Nlcl91c2VyX2FnZW50IjoiTW96aWxsYS81LjAgKFdpbmRvd3MgTlQgMTAuMDsgV09XNjQpIEFwcGxlV2ViS2l0LzUzNy4zNiAoS0hUTUwsIGxpa2UgR2Vja28pIGRpc2NvcmQvMS4wLjkwMTYgQ2hyb21lLzEwOC4wLjUzNTkuMjE1IEVsZWN0cm9uLzIyLjMuMTIgU2FmYXJpLzUzNy4zNiIsImJyb3dzZXJfdmVyc2lvbiI6IjIyLjMuMTIiLCJjbGllbnRfYnVpbGRfbnVtYmVyIjoyMTg2MDQsIm5hdGl2ZV9idWlsZF9udW1iZXIiOjM1MjM2LCJjbGllbnRfZXZlbnRfc291cmNlIjpudWxsfQ==',
     }
 
-    colortable = {
-        "green":"#65fb07",
-        "red":"#Fb0707",
-        "yellow":"#c4bc18",
-        "magenta":"#b207f5",
-        "blue":"#001aff",
-        "cyan":"#07baf5",
-        "gray":"#3a3d40",
-        "white":"#ffffff",
-        "pink":"#c203fc"
-    }
-
