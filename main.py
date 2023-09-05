311bbf0bbc943e4e94c763ee01fd9e75364634b9
PyController <143403968+PyController@users.noreply.github.com>
2023-09-05 20:32:07 +0200

    Update v1.1
    - Added multi-server-joins support
    - Added proxy support
    - Added error handling (And some bypass)

--- main.py
+++ main.py
@@ -1,17 +1,36 @@
 import concurrent.futures
 import random
 import string
+import time
 
 import tls_client
-from colorama import Fore, Style
+from tls_client import Session
+from colorama import Fore, Style, Back
 
 import dtypes
 
 
 class Joiner:
+
+
+    with open("proxies.txt") as prueba:
+        lineas = prueba.readlines()
+        random_proxy = random.choice(lineas)
+
+    prueba_session = Session(
+        client_identifier="opera_90",
+        random_tls_extension_order=True
+    )
+    
+    prueba_session.proxies = {
+        "http": "http://" + random_proxy,
+        "https": "http://" + random_proxy
+    }
+
+    counter = 0
     def __init__(self, data: dtypes.Instance) -> None:
-        self.session = data.client
-        self.session.headers = data.headers
+        self.prueba_session = data.client
+        self.prueba_session.headers = data.headers
         self.get_cookies()
         self.instance = data
 
@@ -19,23 +38,48 @@ class Joiner:
         return ''.join(random.sample(string.ascii_lowercase + string.digits, length))
 
     def get_cookies(self) -> None:
-        site = self.session.get("https://discord.com")
-        self.session.cookies = site.cookies
+        site = self.prueba_session.get("https://discord.com")
+        self.prueba_session.cookies = site.cookies
 
     def join(self) -> None:
-        self.session.headers.update({"Authorization": self.instance.token})
-        result = self.session.post(f"https://discord.com/api/v9/invites/{self.instance.invite}", json={
+        self.prueba_session.headers.update({"Authorization": self.instance.token})
+        result = self.prueba_session.post(f"https://discord.com/api/v9/invites/{self.instance.invite}", json={
             'session_id': self.rand_str(32),
         })
 
         if result.status_code == 200:
-            logger.printk(logger.color('green', f'Joined server: {result.status_code}'))
+            Joiner.counter += 1
+            logger.printk(logger.color('green', f'[{Joiner.counter}] Joined server: {result.status_code}'))
+            time.sleep(2)
 
+        elif result.status_code == 429:
+            logger.printk(logger.color('yellow', "Got captcha, sleeping for 10 seconds..."))
+            time.sleep(10)
+
+        elif result.status_code == 400:
+            logger.printk(logger.color('blue', f"Got cap/ratelimit, sleeping for 20 seconds... {status_code}"))
+            time.sleep(20)
+
+        elif result.status_code == 404:
+            logger.printk(logger.color('blue', f"Got cap/ratelimit, sleeping for 20 seconds... {status_code}"))
+            time.sleep(20)
+        
         else:
-            logger.printk(logger.color('red', result.text))
+            logger.printk(logger.color('red', result))
+            time.sleep(2)
+          
 
 class logger:
-    colors_table = dtypes.OtherInfo.colortable
+    colors_table = {
+        "green": Fore.GREEN,
+        "red": Fore.RED,
+        "yellow": Fore.YELLOW,
+        "magenta": Fore.MAGENTA,
+        "blue": Fore.BLUE,
+        "cyan": Fore.CYAN,
+        "gray": Fore.LIGHTBLACK_EX,
+        "white": Fore.WHITE,
+    }
 
     @staticmethod
     def printk(text) -> None:
@@ -43,43 +87,44 @@ class logger:
 
     @staticmethod
     def convert(color):
-        return color if color.__contains__("#") else logger.colors_table[color]
+        return logger.colors_table[color]
 
     @staticmethod
     def color(opt, obj):
         return f"{logger.convert(opt)}{obj}{Style.RESET_ALL}"
 
+
 class intilize:
     @staticmethod
     def start(i):
         Joiner(i).join()
-        
+
+
+
 if __name__ == '__main__':
-    with open("tokens.txt") as file:
-        tokens = [line.strip() for line in file]
+    with open("tokens.txt") as token_file:
+        tokens = [line.strip() for line in token_file]
+
+    with open("invites.txt") as invite_file:
+        invites = [line.strip() for line in invite_file]
 
     instances = []
-    max_threads = 5
-    invite = input("Discord invite: ")
-    
-    try:
-        invite=invite.split("/")[-1]
-    except:
-        pass
+    max_threads = 1
 
     for token_ in tokens:
         header = dtypes.OtherInfo.headers
-        instances.append(
-            dtypes.Instance(
-                client=tls_client.Session(
-                    client_identifier=f"chrome_{random.randint(110, 115)}",
-                    random_tls_extension_order=True,
-                ),
-                token=token_,
-                headers=header,
-                invite=invite,
+        for invite in invites:
+            instances.append(
+                dtypes.Instance(
+                    client=tls_client.Session(
+                        client_identifier=f"chrome_{random.randint(110, 115)}",
+                        random_tls_extension_order=True,
+                    ),
+                    token=token_,
+                    headers=header,
+                    invite=invite,
+                )
             )
-        )
 
     with concurrent.futures.ThreadPoolExecutor(max_workers=max_threads) as executor:
         for i in instances:
